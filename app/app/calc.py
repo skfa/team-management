3b41aecd660b0560995203c41a5ba2563463c609
Farhad Ahmed <skfa.786@gmail.com>
2024-12-07 23:31:28 +0530

    configure docker to use wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,9 +3,11 @@ Calculator function
 
 """
 
+
 def add(x, y):
     """"Add x and y"""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result"""
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
@@ -13,7 +14,6 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
 
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
